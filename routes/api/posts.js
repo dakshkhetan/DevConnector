cb65536278e5069b53b825d69178814a14649e23
Daksh Khetan <2012daksh@gmail.com>
2020-08-06 23:46:12 +0530

    unlike post (by post ID)

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -132,4 +132,34 @@ router.put('/like/:id', auth, async (req, res) => {
   }
 });
 
+// @route    PUT api/posts/unlike/:id
+// @desc     Like a post
+// @access   Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // prettier-ignore
+
+    // check if the post has already been liked by the user
+    if (post.likes.filter((like) => like.user.toString() === req.user.id).length === 0) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+
+    // get index of the 'like' to be deleted
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    return res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
