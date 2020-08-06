35a43617f0c73cc45cec6f21d4fe66b68730c7b7
Daksh Khetan <2012daksh@gmail.com>
2020-08-07 00:14:01 +0530

    delete comment from post (by comment ID)

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -197,4 +197,40 @@ router.post(
   }
 );
 
+// @route    DELETE api/posts/comment/:id/:comment_id
+// @desc     Delete comment
+// @access   Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // find comment to be deleted
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    // check if comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    // check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // delete comment
+    post.comments = post.comments.filter(
+      (comment) => comment.id !== req.params.comment_id
+    );
+
+    await post.save();
+
+    return res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
