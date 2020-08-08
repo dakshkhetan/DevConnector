0e76bba61b7ba0a0274339ddf6738d001e376005
Daksh Khetan <2012daksh@gmail.com>
2020-08-08 16:44:48 +0530

    create Dashboard component & get current-user profile

--- client/src/redux/reducers/root.reducer.js
+++ client/src/redux/reducers/root.reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import alertReducer from './alert.reducer';
 import authReducer from './auth.reducer';
+import profileReducer from './auth.reducer';
 
 export default combineReducers({
   alert: alertReducer,
-  auth: authReducer
+  auth: authReducer,
+  profile: profileReducer
 });
