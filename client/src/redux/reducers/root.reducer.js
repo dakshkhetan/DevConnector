54baf5db0762b985ac9f0ed0952fe8141974c120
Daksh Khetan <2012daksh@gmail.com>
2020-08-08 16:55:37 +0530

    fix typo

--- client/src/redux/reducers/root.reducer.js
+++ client/src/redux/reducers/root.reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import alertReducer from './alert.reducer';
 import authReducer from './auth.reducer';
-import profileReducer from './auth.reducer';
+import profileReducer from './profile.reducer';
 
 export default combineReducers({
   alert: alertReducer,
