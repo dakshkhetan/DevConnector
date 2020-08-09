52c70436c52471ca5b4f26477f67025bb669d725
Daksh Khetan <2012daksh@gmail.com>
2020-08-09 15:39:56 +0530

    create action to delete education & experience

--- client/src/redux/actions/profile.action.js
+++ client/src/redux/actions/profile.action.js
@@ -141,6 +141,48 @@ export const addEducation = (formData, history) => async (dispatch) => {
   }
 };
 
+export const deleteExperience = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        status: err.response.status,
+        msg: err.response.data.msg
+      }
+    });
+  }
+};
+
+export const deleteEducation = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/profile/education/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Education Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        status: err.response.status,
+        msg: err.response.data.msg
+      }
+    });
+  }
+};
+
 export const deleteAccount = () => async (dispatch) => {
   if (window.confirm('Are you sure? This can NOT be undone!')) {
     try {
