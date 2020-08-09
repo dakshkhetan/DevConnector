94dd7f396109dd6870fc46f3aac6bae60a79ce7e
Daksh Khetan <2012daksh@gmail.com>
2020-08-09 13:38:43 +0530

    add create-profile action

--- client/src/redux/actions/profile.action.js
+++ client/src/redux/actions/profile.action.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
+
 import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { setAlert } from './alert.action';
 
 import setAuthToken from '../../utils/setAuthToken';
 
@@ -16,6 +18,44 @@ export const getCurrentProfile = () => async (dispatch) => {
       payload: res.data
     });
   } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        status: err.response.status,
+        msg: err.response.data.msg
+      }
+    });
+  }
+};
+
+// prettier-ignore
+export const createProfile = (formData, history, edit = false) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
     dispatch({
       type: PROFILE_ERROR,
       payload: {
