4a67865beb638eb0d10a8fec1ac52a5cc0f2776a
Daksh Khetan <2012daksh@gmail.com>
2020-08-10 17:59:33 +0530

    ready for production

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const compression = require('compression');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
@@ -12,19 +13,25 @@ require('dotenv').config({
 connectDB();
 
 app.use(compression());
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
-
-app.get('/', (req, res) => res.send('API Running'));
 
 // routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
