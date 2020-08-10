18a57ef22a5c9cc404a4a894d3453103d9ffbbb7
Daksh Khetan <2012daksh@gmail.com>
2020-08-11 00:57:27 +0530

    logout user on token expiration

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -7,4 +7,21 @@ const api = axios.create({
   }
 });
 
+/**
+ Intercept any error response from the API
+ & check if the token is expired or no longer valid.
+
+ Logout user if the token has expired.
+**/
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response.data.msg === 'Token is not valid') {
+      store.dispatch({ type: LOGOUT });
+    }
+    return Promise.reject(err);
+  }
+);
+
 export default api;
