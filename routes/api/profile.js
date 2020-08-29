0040841ef33063739f89f921c6cf3a0690081d79
Daksh Khetan <2012daksh@gmail.com>
2020-08-29 13:58:48 +0530

    setup route & action to get profile by user ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -122,4 +122,29 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+
+    return res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+
+    return res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
