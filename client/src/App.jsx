9b63364c61180d19f4b17f7afa1ecf31ef7dd883
Daksh Khetan <2012daksh@gmail.com>
2020-09-17 17:35:34 +0530

    add auth routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './App.css';
 
 const App = () => (
-  <div>
-    <Navbar />
-    <Landing />
-  </div>
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </React.Fragment>
+  </Router>
 );
 
 export default App;
