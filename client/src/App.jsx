6bab16019667cb3a310763246f02a3eb230943ca
Daksh Khetan <2012daksh@gmail.com>
2020-09-30 22:16:02 +0530

    load user & set auth token in axios request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './redux/store';
+import { loadUser } from './redux/actions/auth.action';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -11,24 +12,33 @@ import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import setAuthToken from './utils/setAuthToken';
+
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <React.Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Alert />
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </React.Fragment>
-    </Router>
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </React.Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
