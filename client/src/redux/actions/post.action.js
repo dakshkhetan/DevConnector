e67936cb659ecf8826fe125b4da2f014b9d3d211
Daksh Khetan <2012daksh@gmail.com>
2020-10-27 22:46:41 +0530

    implement like & unlike functionality

--- client/src/redux/actions/post.action.js
+++ client/src/redux/actions/post.action.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 export const getPosts = () => async (dispatch) => {
   try {
@@ -8,6 +8,44 @@ export const getPosts = () => async (dispatch) => {
     dispatch({
       type: GET_POSTS,
       payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        status: err.response.status,
+        msg: err.response.data.msg
+      }
+    });
+  }
+};
+
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        status: err.response.status,
+        msg: err.response.data.msg
+      }
+    });
+  }
+};
+
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data }
     });
   } catch (err) {
     dispatch({
